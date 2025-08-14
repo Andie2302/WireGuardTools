0573b280b5fabaa48fcf41e7ca165403fd8cf335
Andreas <Andreas.Zwander@gmail.com>
2025-08-15 00:58:28 +0200

    Create

---
+++ WireGuardTools/Classes/WireGuardKeyPair.cs
@@ -0,0 +1,28 @@
+﻿namespace WireGuardTools.Classes;
+
+public record WireGuardKeyPair
+{
+    public byte[] PrivateKey { get; }
+    public byte[] PublicKey { get; }
+    public string PrivateKeyBase64 { get; }
+    public string PublicKeyBase64 { get; }
+
+    private WireGuardKeyPair(byte[] privateKey, byte[] publicKey)
+    {
+        PrivateKey = privateKey;
+        PublicKey = publicKey;
+        PrivateKeyBase64 = Convert.ToBase64String(PrivateKey);
+        PublicKeyBase64 = Convert.ToBase64String(PublicKey);
+    }
+
+    public static WireGuardKeyPair Create(byte[] privateKey, byte[] publicKey)
+    {
+        if (!CheckKeys.CheckKeyPair(privateKey, publicKey))
+            throw new ArgumentException("Invalid key pair");
+
+        return new WireGuardKeyPair(privateKey, publicKey);
+    }
+
+    // Nur Public Key für Peers (Server kennt nur Client Public Key)
+    public WireGuardPeerKey ToPeerKey() => new(PublicKey);
+}

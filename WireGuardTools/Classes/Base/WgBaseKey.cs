047b9f1daf8c2f814ad543b17c8f36178f118f1d
Andreas <Andreas.Zwander@gmail.com>
2025-08-15 23:17:08 +0200

    Create

--- WireGuardTools/Classes/Base/WgBaseKey.cs
+++ WireGuardTools/Classes/Base/WgBaseKey.cs
@@ -1,15 +1,22 @@
 ﻿namespace WireGuardTools.Classes.Base;
 
-public readonly record struct WgBaseKey ( byte[] Key )
+public readonly record struct WgBaseKey
 {
-    public string KeyBase64 { get; init; } = Convert.ToBase64String ( Key );
+    public const int KeySize = 32;
 
-    public void Deconstruct ( out byte[] key , out string keyBase64 )
+    public WgBaseKey ( byte[] key )
     {
-        key = Key;
-        keyBase64 = KeyBase64;
+        ArgumentNullException.ThrowIfNull ( key );
+
+        if ( key.Length != KeySize ) { throw new ArgumentException ( $"WireGuard keys must be {KeySize} bytes long." , nameof ( key ) ); }
+
+        Key = key;
+        KeyBase64 = Convert.ToBase64String ( key );
     }
 
+    public string KeyBase64 { get; init; }
+    public byte[] Key { get; init; }
     public static implicit operator WgBaseKey ( byte[] key ) => new WgBaseKey ( key );
     public static implicit operator byte[] ( WgBaseKey key ) => key.Key;
+    public void Deconstruct ( out byte[] key ) { key = this.Key; }
 }

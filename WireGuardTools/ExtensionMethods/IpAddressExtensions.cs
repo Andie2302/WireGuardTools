ad7c5f12419a8c7a1396dae8230fb1274a3ffd73
Andreas <Andreas.Zwander@gmail.com>
2025-08-16 15:55:41 +0200

    Check for IpV4 Subnet-Mask

---
+++ WireGuardTools/ExtensionMethods/IpAddressExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace WireGuardTools.ExtensionMethods;
+
+public static class IpAddressExtensions
+{
+    public static bool IsValidIpV4SubnetMask ( this IPAddress address )
+    {
+        if ( address.AddressFamily != AddressFamily.InterNetwork ) { return false; }
+        var addressBytes = address.GetAddressBytes();
+        var maskValue = (uint) ( addressBytes[0] << 24 | addressBytes[1] << 16 | addressBytes[2] << 8 | addressBytes[3] );
+        var invertedValue = ~maskValue;
+        return ( invertedValue + 1 & invertedValue ) == 0;
+
+    }
+}

52c9ffd65baa1688da02773749e9771079018304
Andreas <Andreas.Zwander@gmail.com>
2025-08-30 00:37:02 +0200

    Add Program.cs to generate and display WireGuard key pairs

---
+++ Test/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using WireGuardTools.Generators;
+
+Console.WriteLine("Hello, World!");
+
+for ( var i = 0 ; i < 100 ; i++ )
+{
+    Console.WriteLine($"KeyPair #{i}");
+    var pair = WgKeyPairGenerator.CreateNewWgKeyPair();
+    Console.WriteLine(pair.PrivateKeyAsBase64);
+    Console.WriteLine(pair.PublicKeyAsBase64);
+    Console.WriteLine();
+}

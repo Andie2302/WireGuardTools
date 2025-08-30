bd8c1107fec29acc1b9e38811da1a79c22c0bcbe
Andreas <Andreas.Zwander@gmail.com>
2025-08-30 05:00:02 +0200

    Add `WgTunnel` record struct to represent a WireGuard tunnel with local and remote peers

---
+++ WireGuardTools/WgTunnel.cs
@@ -0,0 +1,8 @@
+﻿namespace WireGuardTools;
+
+public readonly record struct WgTunnel(WgPeer LocalPeer, WgPeer RemotePeer, WgPreSharedKey? PreSharedKey = null)
+{
+    public WgPeer LocalPeer { get; } = LocalPeer;
+    public WgPeer RemotePeer { get; } = RemotePeer;
+    public WgPreSharedKey PreSharedKey { get; } = PreSharedKey??new WgPreSharedKey();
+}

d2ef18b796c0b3506d8bb9e3bdf065eda5dc881a
Andreas <Andreas.Zwander@gmail.com>
2025-08-30 04:59:15 +0200

    Replace `WgKeyPairGenerator.CreateMultipleKeyPairs` with `WgPeerGenerator.Create` in Program.cs

--- Test/Program.cs
+++ Test/Program.cs
@@ -3,7 +3,7 @@
 using WireGuardTools.Generators;
 
 // Generate multiple key pairs efficiently
-foreach (var keyPair in WgKeyPairGenerator.CreateMultipleKeyPairs(5))
+foreach (var keyPair in WgPeerGenerator.Create(5))
 {
     Console.WriteLine($"Private: {keyPair.PrivateKeyAsBase64}");
     Console.WriteLine($"Public:  {keyPair.PublicKeyAsBase64}");

2a15ab195eab65da3aba8a4913882bcc9345326f
Andreas <Andreas.Zwander@gmail.com>
2025-09-18 01:23:30 +0200

    Add `WireGuardToolException` to handle WireGuard tool operation failures

---
+++ WireGuardTools/test/WireGuardToolException.cs
@@ -0,0 +1,10 @@
+﻿namespace WireGuardTools.test;
+
+/// <summary>
+/// Exception thrown when WireGuard tool operations fail
+/// </summary>
+public class WireGuardToolException : Exception
+{
+    public WireGuardToolException(string message) : base(message) { }
+    public WireGuardToolException(string message, Exception innerException) : base(message, innerException) { }
+}

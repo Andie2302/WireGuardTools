2bc08a544ed177499e80880924562d70bc7ff80c
Andreas <Andreas.Zwander@gmail.com>
2025-09-17 21:27:29 +0200

    Add `KeySize` and `DefaultPort` constants to `WgTools` for standardization.

--- WireGuardTools/WgTools.cs
+++ WireGuardTools/WgTools.cs
@@ -2,4 +2,15 @@
 
 public static class WgTools
 {
+    /// <summary>
+    /// The standard key size for WireGuard keys (32 bytes / 256 bits)
+    /// </summary>
+    public const int KeySize = 32;
+
+    /// <summary>
+    /// The standard port for WireGuard (51820)
+    /// </summary>
+    public const int DefaultPort = 51820;
+
+
 }

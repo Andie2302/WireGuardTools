bf5a63135c6186d0823a86eade181f539fcfe323
Andreas <Andreas.Zwander@gmail.com>
2025-09-17 21:15:28 +0200

    Refactor `Curve25519KeyPairGenerator` to use `ECDiffieHellman` for key generation.

--- WireGuardTools/Curve25519KeyPairGenerator.cs
+++ WireGuardTools/Curve25519KeyPairGenerator.cs
@@ -1,24 +1,15 @@
-﻿namespace WireGuardTools;
+﻿using System.Security.Cryptography;
 
-public static WgKeyPair Generate() => new(new Curve25519KeyPairGenerator());
-public static WgKeyPair FromPrivateKey(WgKey privateKey) => new(privateKey, new Curve25519KeyPairGenerator());
-
-
+namespace WireGuardTools;
 
 public class Curve25519KeyPairGenerator : IWgKeyPairGenerator
 {
-
-    public WgKey GeneratePublicKeyFromPrivate(WgKey privateKey)
-    {
-        // Curve25519 Magic hier
-        byte[] publicKeyBytes = GeneratePublicKeyFromPrivate(privateKey.Key);
-        return new WgKey(publicKeyBytes);
-    }
-
     public WgKeyPair GenerateKeyPair()
     {
-        var privateKey = WgKey.CreateRandom();
-        var publicKey = GeneratePublicKeyFromPrivate(privateKey);
-        return new WgKeyPair(privateKey, publicKey);
+        using var ecdh = ECDiffieHellman.Create(WgCurve.Curve25519);
+        var ecParameters = ecdh.ExportParameters(true);
+        if (ecParameters.D is null) throw new Exception("D is null");
+        if (ecParameters.Q.X is null) throw new Exception("Q.X is null");
+        return ecParameters.Q.Y is null ? throw new Exception("Q.Y is null") : new WgKeyPair(new WgKey(ecParameters.D), new WgKey(ecParameters.Q.X));
     }
 }

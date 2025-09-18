058dd51759c74b0ab76b89f41568d6743db0a30d
Andreas <Andreas.Zwander@gmail.com>
2025-09-18 02:04:01 +0200

    Add `WgTunnel` to model key pair and pre-shared key relationships

---
+++ WireGuardTools/WgTunnel.cs
@@ -0,0 +1,9 @@
+﻿namespace WireGuardTools;
+
+public class WgTunnel(WgKeyPair serverToClient, WgKeyPair clientToServer, WgKey preSharedKey)
+{
+    public WgKeyPair ServerToClient { get; } = serverToClient ?? throw new ArgumentNullException(nameof(serverToClient));
+    public WgKeyPair ClientToServer { get; } = clientToServer ?? throw new ArgumentNullException(nameof(clientToServer));
+    public WgKey PreSharedKey { get; } = preSharedKey ?? throw new ArgumentNullException(nameof(preSharedKey));
+    public override string ToString() => $"ServerToClient: {ServerToClient}, ClientToServer: {ClientToServer}, PreSharedKey: {PreSharedKey}";
+}

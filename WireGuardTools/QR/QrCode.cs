19b18e9b0300141c16bc8d49c675352b829eaf9f
Andreas <Andreas.Zwander@gmail.com>
2025-09-18 13:35:03 +0200

    Add `QrCode` utility to generate QR codes as PNGs using `QRCoder`.

---
+++ WireGuardTools/QR/QrCode.cs
@@ -0,0 +1,30 @@
+﻿using QRCoder;
+
+namespace WireGuardTools.QR;
+
+public static class QrCode
+{
+    public static void GenerateQrCodePng(string configContent, string filePath)
+    {
+        try
+        {
+            using (var qrGenerator = new QRCodeGenerator())
+            {
+                using (var qrCodeData = qrGenerator.CreateQrCode(configContent, QRCodeGenerator.ECCLevel.Q))
+                {
+                    using (var qrCode = new PngByteQRCode(qrCodeData))
+                    {
+                        var qrCodeBytes = qrCode.GetGraphic(20);
+                        File.WriteAllBytes(filePath, qrCodeBytes);
+                    }
+                }
+            }
+
+            Console.WriteLine($"QR-Code als PNG in '{filePath}' gespeichert.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ein Fehler ist aufgetreten: {ex.Message}");
+        }
+    }
+}

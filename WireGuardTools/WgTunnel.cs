79eaf9ffa94b1c9b96e902c2d3975f027c043447
Andreas <Andreas.Zwander@gmail.com>
2025-09-18 02:21:36 +0200

    Add static factory methods to `WgTunnel` for key pair generation and batch creation

--- WireGuardTools/WgTunnel.cs
+++ WireGuardTools/WgTunnel.cs
@@ -6,4 +6,9 @@ public class WgTunnel(WgKeyPair serverToClient, WgKeyPair clientToServer, WgKey 
     public WgKeyPair ClientToServer { get; } = clientToServer ?? throw new ArgumentNullException(nameof(clientToServer));
     public WgKey PreSharedKey { get; } = preSharedKey ?? throw new ArgumentNullException(nameof(preSharedKey));
     public override string ToString() => $"ServerToClient: {ServerToClient}, ClientToServer: {ClientToServer}, PreSharedKey: {PreSharedKey}";
+    public static WgTunnel Create(WgKeyPair serverToClient, WgKeyPair clientToServer, WgKey preSharedKey) => new(serverToClient, clientToServer, preSharedKey);
+    public static WgTunnel CreateRandom(IWgKeyPairGenerator keyPairGenerator) => Create(keyPairGenerator.GenerateKeyPair(), keyPairGenerator.GenerateKeyPair(), WgKey.CreateRandom());
+    public static WgTunnel CreateRandom() => CreateRandom(new Curve25519KeyPairGenerator());
+    public static IEnumerable<WgTunnel> CreateRandom(IWgKeyPairGenerator keyPairGenerator, int count) => Enumerable.Range(0, count).Select(_ => CreateRandom(keyPairGenerator));
+    public static IEnumerable<WgTunnel> CreateRandom(int count) => CreateRandom(new Curve25519KeyPairGenerator(), count);
 }
